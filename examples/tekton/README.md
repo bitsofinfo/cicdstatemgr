1a59ac954c71dce5f0865290eb221a14bf9cfe17
bitsofinfo <bitsofinfo.g@gmail.com>
2020-07-24 16:50:42 -0400

    tekton example

--- examples/tekton/README.md
+++ examples/tekton/README.md
@@ -1,1 +1,25 @@
 # tekton example
+
+```
+cd k8s
+./install.sh
+```
+
+
+Fork https://github.com/bitsofinfo/nginx-hello-world
+
+* settings -> webhooks -> add webhook
+
+
+
+
+
+kubectl run -it --serviceaccount=cicd-tekton --namespace tekton-pipelines  cicd-toolbox --image bitsofinfo/cicd-toolbox:latest /bin/bash 
+
+
+kubectl get svc registry --output=jsonpath='{.spec.clusterIP}' --namespace=kube-system
+
+
+https://github.com/kameshsampath/minikube-helpers/blob/master/registry/README.md
+
+https://developers.redhat.com/blog/2019/07/11/deploying-an-internal-container-registry-with-minikube-add-ons/
---
+++ examples/tekton/install.sh
@@ -0,0 +1,94 @@
+#!/bin/bash
+
+cd core
+./install.sh
+
+cd ../pipelines
+./install.sh
+
+cd ..
+
+echo
+echo "Waiting for services to come up "
+READY=1
+while [ "$READY" != "0" ]; do
+    sleep 1
+    CAPTURE=$(kubectl get pods | grep el-event-listener-v1 | grep Running)
+    READY=$?
+    echo -n "."
+done
+
+sleep 5
+
+EVENT_LISTENER_TUNNEL_URI=$(minikube service el-event-listener-v1 --url -n tekton-pipelines)
+DASHBOARD_TUNNEL_URI=$(minikube service tekton-dashboard --url -n tekton-pipelines)
+
+echo
+echo
+
+#exit 0
+
+if [ ! -z "$EVENT_LISTENER_TUNNEL_URI" ]; then
+    EVENT_LISTENER_TUNNEL_URI_IP_PORT="${EVENT_LISTENER_TUNNEL_URI#http://}"
+    NGROK_CMD="ngrok http $EVENT_LISTENER_TUNNEL_URI_IP_PORT &>/dev/null"
+    echo "Starting ngrok: cmd: $NGROK_CMD ...."
+    echo "Starting ngrok: cmd: $NGROK_CMD ...."
+    echo "Starting ngrok: cmd: $NGROK_CMD ...."
+    echo "$NGROK_CMD"
+    eval "($NGROK_CMD) &"
+    NGROK_PID=$!
+    sleep 5
+    NGROK_URL=$(curl -s http://localhost:4040/api/tunnels | jq .tunnels[0].public_url)
+    NGROK_URL="${NGROK_URL%\"}"
+    NGROK_URL="${NGROK_URL#\"}"
+    echo
+    echo "Local EventListener: $EVENT_LISTENER_TUNNEL_URI"
+    echo "Public NGROK EventListener: $NGROK_URL"
+    echo "Kill with: kill -TERM $NGROK_PID"
+    echo
+else
+    echo
+    echo "You should run the following in a separate terminal:"
+    echo ""
+    echo "  minikube service el-event-listener-v1 --url -n tekton-pipelines"
+    echo
+    echo "Once up, grab the port for the el-event-listener-v1 service exposed locally and pass it to ngrok"
+    echo
+    echo "  ngrok http -log-level \"debug\" [port]"
+    echo ""
+fi
+
+if [ ! -z "$DASHBOARD_TUNNEL_URI" ]; then
+    echo "Tekton dashboard: $DASHBOARD_TUNNEL_URI"
+    echo
+else
+    echo
+    echo "You should run the following in a separate terminal:"
+    echo ""
+    echo "  minikube service tekton-dashboard --url -n tekton-pipelines"
+fi
+
+echo
+echo "Finally go to your fork of https://github.com/bitsofinfo/nginx-hello-world and go to Setting > Webhooks"
+echo "and add new new webhook with the following settings:"
+echo
+echo "  Payload URL = your ngrok URL"
+echo "  Context type = application/json"
+echo "  Secret = 123"
+echo ""
+echo "Next you can trigger a build by pushing a tag from within your nginx-hello-world fork"
+echo "  git tag -a [tag] -m \"test\"; git push origin [tag]"
+echo ""
+echo "After pushing a new tag, go to your tekton-dashboard to view whats going on"
+echo ""
+echo "Slack notifications are emitted to the bitsofinfo.slack.com #cicdstatemgr-example channel:"
+echo "  https://app.slack.com/client/TE2KJDF4L/CE46X7NQN"
+echo
+echo "Your tag of nginx-hello-world should be deployed, lets hit it:"
+echo "  minikube service list"
+echo "  minikube service nginx-hello-world-[tag] --url -n apps-dev"
+echo ""
+echo "  curl http://localhost:[port]"
+echo "  You should see 'nginx-hello-world:[tag]] is running within environment: apps-dev'"
+echo
+echo
