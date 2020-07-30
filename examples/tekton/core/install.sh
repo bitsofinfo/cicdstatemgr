044d6ba44aee6cdd20acb88fbfa09af6c2002a66
bitsofinfo <bitsofinfo.g@gmail.com>
2020-07-30 09:06:44 -0400

    latest tekton components

--- examples/tekton/core/install.sh
+++ examples/tekton/core/install.sh
@@ -25,7 +25,7 @@ kubectl create namespace tekton-pipelines
 kubectl config set-context --current --namespace=tekton-pipelines
 
 # install pipelines
-kubectl apply -f https://storage.googleapis.com/tekton-releases/pipeline/previous/v0.14.2/release.yaml
+kubectl apply -f https://storage.googleapis.com/tekton-releases/pipeline/previous/v0.14.3/release.yaml
 
 # install pipelines customizations (configmaps)
 # - feature-flags 
@@ -36,7 +36,7 @@ kubectl apply -f $SCRIPTPATH/tekton-pipelines-mods.yaml
 kubectl apply -f https://storage.googleapis.com/tekton-releases/triggers/previous/v0.6.1/release.yaml
 
 # install dashboard
-kubectl apply --filename https://github.com/tektoncd/dashboard/releases/download/v0.7.1/tekton-dashboard-release.yaml
+kubectl apply --filename https://github.com/tektoncd/dashboard/releases/download/v0.8.2/tekton-dashboard-release.yaml
 
 # NodePort change
 kubectl apply -f $SCRIPTPATH/tekton-dashboard-mods.yaml
